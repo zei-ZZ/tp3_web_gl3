7e46014110f99abd452abbb213e993128e9e1856
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-05-08 01:12:23 +0100

    refactor: use the CV constant in the EventType
    Co-authored-by: Zeineb Benabdallah <88910661+zei-ZZ@users.noreply.github.com>

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -2,26 +2,32 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { UserEntity } from '../../auth/entities/user.entity';
 import { CvEntity } from '../../cvs/entities/cv.entity';
 
-export enum EventType {
-  ADD = 'cv.add',
-  DELETE = 'cv.delete',
-  UPDATE = 'cv.update',
-}
 export const CV = 'cv';
+
+export enum EventType {
+  ADD = `${CV}.add`,
+  DELETE = `${CV}.delete`,
+  UPDATE = `${CV}.update`,
+}
+
 @Entity('event')
 export class EventEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column({ type: 'enum', enum: EventType })
   type: EventType;
+
   @ManyToOne(() => UserEntity)
   user: UserEntity;
+
+  @Column(() => CvEntity)
+  cv: CvEntity;
+
   @Column({
     name: 'created_at',
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
   })
   createdAt: Date;
-  @Column(() => CvEntity)
-  cv: CvEntity;
 }
