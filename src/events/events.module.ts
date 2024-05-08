3acec86aa315be230fba019bb7de26edc5ec2c13
Mohamed Hedi Ben Ali <94202725+medhedibenali@users.noreply.github.com>
2024-05-08 01:09:36 +0100

    fix: add wildcards and register the event listener
    Co-authored-by: Zeineb Benabdallah <88910661+zei-ZZ@users.noreply.github.com>

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,10 +3,11 @@ import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventEntity } from './entities/event.entity';
+import { EventListener } from './event.listener';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EventEntity])],
   controllers: [EventsController],
-  providers: [EventsService],
+  providers: [EventsService, EventListener],
 })
 export class EventsModule {}
